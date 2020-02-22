ea000b5c94e8f6595d34635b1e6563f60a6d79ae
Anders Jenbo <anders@jenbo.dk>
2020-02-22 18:28:39 +0100

    Fix audio in SDL (borken in 2.0.10 only)

--- prepare.sh
+++ prepare.sh
@@ -5,6 +5,7 @@ cp ../Packaging/resources/CharisSILB.ttf app/src/main/assets
 wget http://libsdl.org/release/SDL2-2.0.10.tar.gz
 tar -xzf SDL2-2.0.10.tar.gz
 rm SDL2-2.0.10/CMakeLists.txt
+rm SDL2-2.0.10/src/audio/SDL_audiotypecvt.c
 mv SDL2-2.0.10/* SDL2
 rm -rf SDL2-2.0.10 SDL2-2.0.10.tar.gz
 
